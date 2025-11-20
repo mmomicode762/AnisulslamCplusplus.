bd042c1103f9d7bdbabae83915621ae9814609e0
Mahmuda Akter Momi <mmomi2410013@bscse.uiu.ac.bd>
2025-11-20 13:23:18 +0600

    Add files via upload

---
+++ demo2.cpp
@@ -0,0 +1,20 @@
+// c++ tokens(keywords,constants,identifier,strings,special symbols,operators
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+
+
+  int num1=10;
+  int num2=20;
+  char ch='a';
+
+  cout<<num1<<endl;
+  cout<<num2<<endl;
+
+  cout<< "Number1= "<<num1<<endl<<"Number2= "<<num2 <<endl;
+  cout<<"character = "<< ch;
+
+
+ getch();
+}
