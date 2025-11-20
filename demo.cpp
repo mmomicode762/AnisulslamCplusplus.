c78894a5518454cff6688c03258ca4496168dd1a
Mahmuda Akter Momi <mmomi2410013@bscse.uiu.ac.bd>
2025-11-20 13:21:42 +0600

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+//input output stream one header file.
+using namespace std;
+
+int main(){
+
+  cout<<"Hello World!";//cout -print Standard output stream , main- function ()
+  return 0;
+    // C++ is the very powerful computer programming language.
+    //It is a case sensitive,object-oriented ,general purpose language.
+    //usage of C++--- develop of game engines,games,desktop app,art applications,music players etc. and device driver code
+    //It was developed in 1980 by Bjarne Stroustrup(father of C++) at bell laboratories .
+    //c++ is the mid level programming language.
+    //program is the computer language(0,1).
+
+
+}
