bafc88ff3cee69e2d2f6cc4eeef48e715db81cc6
Mahmuda Akter Momi <mmomi2410013@bscse.uiu.ac.bd>
2025-11-20 13:22:31 +0600

    Add files via upload

---
+++ demo1.cpp
@@ -0,0 +1,11 @@
+// biodata printing..
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+// printing my name ,phn number, address.
+cout<<"Mahmuda Akter Momi"<<endl<<"Rajshahi"<<endl<<"01326769163";
+getch();
+}
